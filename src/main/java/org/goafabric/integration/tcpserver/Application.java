82f97216632d8e64dff0f747228c89cb04baae2d
Andreas Mautsch <amautsch>
2023-02-06 15:57:35 +0100

    refact

--- src/main/java/org/goafabric/integration/tcpserver/Application.java
+++ src/main/java/org/goafabric/integration/tcpserver/Application.java
@@ -1,7 +1,11 @@
 package org.goafabric.integration.tcpserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.MessageChannel;
 
 
 /**
@@ -14,5 +18,12 @@ public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
+    @Bean
+    public CommandLineRunner init(ApplicationContext context, MessageChannel myChannel) {
+       context.getBean(MyConfiguration.class).createMessage();
+        //IntegrationConfiguration.integration(kafkaChannel);
+        return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
+    }
+
 
 }
--- src/main/java/org/goafabric/integration/tcpserver/MyConfiguration.java
+++ src/main/java/org/goafabric/integration/tcpserver/MyConfiguration.java
@@ -2,11 +2,15 @@ package org.goafabric.integration.tcpserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.integration.dsl.Pollers;
+import org.springframework.messaging.*;
+import org.springframework.messaging.support.MessageBuilder;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
@@ -27,5 +31,28 @@ public class MyConfiguration {
                 .transform(Object::toString)
                 .channel(MessageChannels.queue())
                 .get();
+    }
+
+    @Bean
+    public IntegrationFlow inputFlow() {
+        return IntegrationFlow.from("application.myChannel")
+                .channel(MessageChannels.queue())
+                .handle(new MessageHandler() {
+                    @Override
+                    public void handleMessage(Message<?> message) throws MessagingException {
+                        System.err.println(message);
+                    }
+                })
+                .get();
+    }
+
+    @Bean
+    public MessageChannel myChannel() {
+        return new DirectChannel();
+    }
+
+    public void createMessage() {
+        var message = MessageBuilder.createMessage(new Object(), new MessageHeaders(Map.of("yo", "")));
+        myChannel().send(message);
     }
 }
