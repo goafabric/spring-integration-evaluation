e3a5d6fc30699205a9c3cb8d37b4ec26b2bfa789
Andreas Mautsch <amautsch>
2023-02-08 09:13:09 +0100

    refact

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # description 
-- based on https://github.com/spring-projects/spring-integration-samples/tree/main/basic/tcp-client-server
-- but heavily stripped down and rework to spring boot 
+- a bunch of examples for spring integration java dsl
+
+# simple
+- simple example with just channels and logging
           
-# simple test from command line
-echo -n "hello\r\n" | nc 127.0.0.1 8000
+# tcp
+- based on https://github.com/spring-projects/spring-integration-samples/tree/main/basic/tcp-client-server
+- echo -n "hello\r\n" | nc 127.0.0.1 8000
--- src/main/java/org/goafabric/integration/tcpserver/SimpleConfiguration.java
+++ src/main/java/org/goafabric/integration/tcpserver/SimpleConfiguration.java
@@ -3,40 +3,41 @@ package org.goafabric.integration.tcpserver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.integration.channel.QueueChannel;
-import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.GenericMessage;
 
-@Configuration
-@EnableIntegration
 @Slf4j
-public class MyConfiguration {
+@Configuration
+//@EnableIntegration
+@Profile("simple")
+public class SimpleConfiguration {
 
+    //this is what Integration actually calls an OutboundAdapter or @ServiceActivator(inputChannel =  "logChannel") in not DSL
     @Bean
     public IntegrationFlow logMessage() {
-        return IntegrationFlow.from(myChannel())
+        return IntegrationFlow.from(logChannel())
                 .handle(message -> log.info("## got message " + message.getPayload()))
                 .get();
     }
 
     /*
-    @ServiceActivator(inputChannel =  "myChannel")
+    @ServiceActivator(inputChannel =  "lobChannel")
     void logMessage(Object payload) { log.info(payload.toString()); }
     */
 
-
     @Bean
-    public MessageChannel myChannel() { return new QueueChannel(); }
+    public MessageChannel logChannel() { return new QueueChannel(); }  // don't dare to use DirectChannel it will fail
 
     @Bean
     public MessageChannel myOtherChannel() { return new QueueChannel(); }
 
     @Bean
     public void testMe() {
-        myChannel().send(new GenericMessage<>("hit me baby"));
-        myChannel().send(new GenericMessage<>("one more time"));
+        logChannel().send(new GenericMessage<>("hit me baby"));
+        logChannel().send(new GenericMessage<>("one more time"));
         myOtherChannel().send(new GenericMessage<>("do not route me"));
     }
 
