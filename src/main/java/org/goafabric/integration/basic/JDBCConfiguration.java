9651c7fc10a8f4ebaba83b9b817f6a3ec87a1918
Andreas Mautsch <amautsch>
2023-02-09 10:53:26 +0100

    jdbc changes

--- src/main/java/org/goafabric/integration/basic/JDBCConfiguration.java
+++ src/main/java/org/goafabric/integration/basic/JDBCConfiguration.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.jdbc.JdbcPollingChannelAdapter;
+import org.springframework.messaging.MessageChannel;
 
 import javax.sql.DataSource;
 
@@ -25,18 +27,23 @@ public class JDBCConfiguration {
         return IntegrationFlow.from(messageSource,
                         c -> c.poller(Pollers.fixedRate(1000).maxMessagesPerPoll(1)))
                 //.transform(Transformers.toJson())
-                .channel("jdbcChannel")
+                .channel(jdbcChannel())
                 .get();
     }
 
     @Bean
     public IntegrationFlow outputFlow() {
-        return IntegrationFlow.from("jdbcChannel")
+        return IntegrationFlow.from(jdbcChannel())
                 .handle(message -> {
                             log.info("## got message " + message.getPayload());
                         }
                 )
                 .get();
+    }
+
+    @Bean
+    public MessageChannel jdbcChannel() {
+        return new DirectChannel();
     }
 
 
