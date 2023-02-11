524f4f2c24d04751f7c28297bf6821fe86e01695
Andreas Mautsch <amautsch>
2023-02-11 17:17:50 +0100

    calleeservice added

--- src/main/java/org/goafabric/integration/complex/TcpConfiguration.java
+++ src/main/java/org/goafabric/integration/complex/TcpConfiguration.java
@@ -1,4 +1,4 @@
-package org.goafabric.integration.basic;
+package org.goafabric.integration.complex;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +10,14 @@ import org.springframework.integration.dsl.Transformers;
 import org.springframework.integration.ip.dsl.Tcp;
 import org.springframework.integration.ip.tcp.serializer.ByteArrayCrLfSerializer;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Slf4j
 @Configuration
+@EnableWebSocketMessageBroker
 @Profile("tcp")
 public class TcpConfiguration {
     final int port = 5001;
@@ -27,10 +32,23 @@ public class TcpConfiguration {
     }
 
     @Bean
-    public IntegrationFlow outputFlow() {
+    public IntegrationFlow outputFlow(SimpMessageSendingOperations template) {
         return IntegrationFlow.from(tcpChannel())
-                .handle(message -> log.info("## got message " + message.getPayload()))
+                .handle(message -> {
+                    log.info("## got message " + message.getPayload());
+                    template.convertAndSend("/public", message.getPayload());
+                })
                 .get();
+    }
+
+    @Bean
+    public WebSocketMessageBrokerConfigurer webSocketConfig() {
+        return new WebSocketMessageBrokerConfigurer() {
+            @Override
+            public void registerStompEndpoints(StompEndpointRegistry registry) {
+                registry.addEndpoint("/websocket").withSockJS();
+            }
+        };
     }
 
     @Bean
