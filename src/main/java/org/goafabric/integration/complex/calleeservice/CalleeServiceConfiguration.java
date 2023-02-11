1fc16306d8be860dd3820d94ce16b410a75482e8
Andreas Mautsch <amautsch>
2023-02-11 10:44:03 +0100

    calleeservice added

---
+++ src/main/java/org/goafabric/integration/complex/calleeservice/CalleeServiceConfiguration.java
@@ -0,0 +1,30 @@
+package org.goafabric.integration.complex.calleeservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.http.dsl.Http;
+
+@Slf4j
+@Configuration
+@Profile("service")
+public class CalleeServiceConfiguration {
+    //curl http://localhost:50900/callees/sayMyName
+    @Bean
+    public IntegrationFlow inputFlow() {
+        var messageSource = Http.inboundChannelAdapter("/callees/sayMyName")
+                .requestMapping(r -> r.methods(HttpMethod.GET));
+        return IntegrationFlow.from(messageSource)
+                .channel("httpChannel") //if we use just a literal, channel will be created automatically
+                .get();
+    }
+    @Bean
+    public IntegrationFlow outputFlow() {
+        return IntegrationFlow.from("httpChannel")
+                .handle(message -> log.info("## got message " + message.getPayload()))
+                .get();
+    }
+}
