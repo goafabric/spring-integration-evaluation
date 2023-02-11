6b2d06fa571388a882fe18b2d630518eda44c63a
Andreas Mautsch <amautsch>
2023-02-11 11:14:19 +0100

    calleeservice added

--- src/main/java/org/goafabric/integration/complex/calleeservice/CalleeServiceConfiguration.java
+++ src/main/java/org/goafabric/integration/complex/calleeservice/CalleeServiceConfiguration.java
@@ -7,25 +7,20 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.http.dsl.Http;
+import org.springframework.util.LinkedMultiValueMap;
 
 @Slf4j
 @Configuration
 @Profile("service")
 public class CalleeServiceConfiguration {
-    //curl http://localhost:50900/callees/sayMyName?name=slim
+    //curl -m 1 "http://localhost:50900/callees/sayMyName?name=yo"
     @Bean
-    public IntegrationFlow inputFlow() {
+    public IntegrationFlow sayMyName() {
         var messageSource = Http.inboundChannelAdapter("/callees/sayMyName")
                 .requestMapping(r -> r.methods(HttpMethod.GET).params("name"));
 
         return IntegrationFlow.from(messageSource)
-                .channel("httpChannel") //if we use just a literal, channel will be created automatically
-                .get();
-    }
-    @Bean
-    public IntegrationFlow outputFlow() {
-        return IntegrationFlow.from("httpChannel")
-                .handle(message -> log.info("## got message " + message.getPayload()))
+                .handle(message -> log.info("your name is : " + ((LinkedMultiValueMap)message.getPayload()).get("name")))
                 .get();
     }
 }
